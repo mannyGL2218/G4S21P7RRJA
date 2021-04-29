b53830c4db16b7e21166466e8baff802f78180d2
manny <manny@mannyGL>
2021-04-29 12:00:32 -0500

    Mostrar ArrayList y formulario con tecla ENTER

---
+++ src/g4s21p7rrja/G4S21P7RRJA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21p7rrja;
+
+/**
+ *
+ * @author manny
+ */
+public class G4S21P7RRJA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmPrincipal frmprincipal = new FrmPrincipal();
+        frmprincipal.setVisible(true);
+    }
+    
+}
